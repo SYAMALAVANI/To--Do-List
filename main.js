c7d96c343d38428bc15f387cee0a7c97eb6f68e6
SYAMALAVANI <78420988+SYAMALAVANI@users.noreply.github.com>
2023-07-04 14:05:41 +0530

    First Commit

---
+++ main.js
@@ -0,0 +1,34 @@
+let inputBox = document.getElementById("input-box");
+let listContainer = document.getElementById("list-container");
+
+function addTask(){
+  if (inputBox.value === ""){
+    alert("You must write something!");
+  }
+  else{
+    let li = document.createElement("li");
+    li.innerText = inputBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerText = "\u00d7";
+    li.appendChild(span);
+    
+  }
+  inputBox.value= "";
+
+}
+
+
+listContainer.addEventListener("click", function(e) {
+  if (e.target.tagName === "LI") {
+    e.target.classList.toggle("checked");
+
+  }
+  else if (e.target.tagName === "SPAN") {
+    e.target.parentElement.remove();
+
+  }
+}, false);
+
+
+
